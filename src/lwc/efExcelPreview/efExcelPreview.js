55ed6515d3dcc2a298df607775c4cef100d59fed
CloudBudget Inc <68500411+CloudBudgetInc@users.noreply.github.com>
2023-10-22 15:34:41 +0200

    Initial commit

---
+++ src/lwc/efExcelPreview/efExcelPreview.js
@@ -0,0 +1,114 @@
+/**
+Copyright (c) 10 2023, AJR
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright notice,
+this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+ * Neither the name of the CloudBudget, Inc. nor the names of its contributors
+may be used to endorse or promote products derived from this software
+without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+import {LightningElement, track} from 'lwc';
+import {_message, _parseServerError} from "c/efUtils";
+import getEFTemplateByIdServer from '@salesforce/apex/EFTemplateSelector.getEFTemplateByIdServer';
+import getEFDataSetsByEFTemplateIdServer from '@salesforce/apex/EFDataSetSelector.getEFDataSetsByEFTemplateIdServer';
+import getAllEFSheetsByTemplateIdServer from '@salesforce/apex/EFSheetSelector.getAllEFSheetsByTemplateIdServer';
+import getAllEFColumnsByTemplateIdServer from '@salesforce/apex/EFColumnSelector.getAllEFColumnsByTemplateIdServer';
+import getAllEFRowsByTemplateIdServer from '@salesforce/apex/EFRowSelector.getAllEFRowsByTemplateIdServer';
+import getAllEFCellsByTemplateIdServer from '@salesforce/apex/EFCellSelector.getAllEFCellsByTemplateIdServer';
+import getAllEFStylesByTemplateIdServer from '@salesforce/apex/EFStyleSelector.getAllEFStylesByTemplateIdServer';
+import getSObjectsByEFDataSetIdServer from '@salesforce/apex/EFSObjectSelector.getSObjectsByEFDataSetIdServer';
+
+export default class EFExcelPreview extends LightningElement {
+
+	@track testMessage = 'N/A';
+	@track showSpinner = false;
+	@track showTable = false;
+	@track recordId = 'a060700000D913SAAR';
+
+	@track template = {};
+	@track dSets = [];
+	@track sheets = [];
+	@track columns = [];
+	@track rows = [];
+	@track cells = [];
+	@track styles = [];
+
+	@track sObjectsMap = {}; // key is dataSet Id, value is a list ov sObjects
+
+	@track tableStructure = {};
+
+	async connectedCallback() {
+		this.doInit();
+	};
+
+	doInit = async () => {
+		try {
+			this.showSpinner = true;
+			this.tableStructure = {};
+			await this.getTemplate();
+			console.log('template' + JSON.stringify(this.template));
+			await this.getDataSets();
+			console.log('dSets' + JSON.stringify(this.dSets));
+			await this.getSheets();
+			console.log('sheets' + JSON.stringify(this.sheets));
+			await this.getColumns();
+			console.log('columns' + JSON.stringify(this.columns));
+			await this.getRows();
+			console.log('rows' + JSON.stringify(this.rows));
+			await this.getCells();
+			console.log('cells' + JSON.stringify(this.cells));
+			await this.getStyles();
+			console.log('styles' + JSON.stringify(this.styles));
+
+			await this.getSObjectMap();
+			console.log('sObjectMap' + JSON.stringify(this.sObjectsMap));
+
+			this.tableStructure = ['template', 'dSets', 'sheets', 'columns', 'rows', 'cells', 'styles'].reduce((r, f) => {
+				r[f] = this[f];
+				return r;
+			}, {});
+			console.log(JSON.stringify(this.tableStructure));
+			this.showTable = true;
+			this.showSpinner = false;
+		} catch (e) {
+			_message('error', 'DO Init Error: ' + e);
+		}
+	};
+
+	getTemplate = async () => this.template = await getEFTemplateByIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Template Error : ', e));
+	getDataSets = async () => this.dSets = await getEFDataSetsByEFTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Data Sets Error : ', e));
+	getSheets = async () => this.sheets = await getAllEFSheetsByTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Sheets Error : ', e));
+	getColumns = async () => this.columns = await getAllEFColumnsByTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Columns Error : ', e));
+	getRows = async () => this.rows = await getAllEFRowsByTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Rows Error : ', e));
+	getCells = async () => this.cells = await getAllEFCellsByTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Cells Error : ', e));
+	getStyles = async () => this.styles = await getAllEFStylesByTemplateIdServer({tId: this.recordId}).catch(e => _parseServerError('Get Styles Error : ', e));
+
+	getSObjectMap = async () => {
+		for (let i = 0; i < this.dSets.length; i++) {
+			await this.getSObjects(this.dSets[i].Id);
+		}
+	};
+
+	getSObjects = async (dsId) => {
+		await getSObjectsByEFDataSetIdServer({dsId})
+			.then(sObjects => this.sObjectsMap[dsId] = sObjects)
+			.catch(e => _parseServerError('Get sObjects Error : ' + e))
+	};
+
+}
---
+++ src/lwc/efExcelTable/efExcelStructure.js
@@ -0,0 +1,164 @@
+import {_message} from "c/efUtils";
+
+let c;
+let styleMap = {}; // key is style Id, value is EFStyle object
+const sheets = [];
+const MIN_SHEET_WIDTH = 20;
+const MIN_SHEET_HEIGHT = 20;
+const defaultColumnWidth = 75;
+
+const setContext = (context) => {
+	c = context;
+};
+
+const generateTable = () => {
+	try {
+		styleMap = c.styles.reduce((r, st) => {
+			r[st.Id] = st;
+			return r;
+		}, {});
+		console.log('Style Map = ' + JSON.stringify(styleMap));
+		c.sheets.forEach(sheet => generateSheet(sheet));
+	} catch (e) {
+		_message('error', 'Generate Table Error : ' + e);
+	}
+};
+
+const generateSheet = (sheet) => {
+	try {
+		const tableSheet = {letterHeaders: [], rows: []}; // list of rows
+		const sheetId = sheet.Id.substring(0, 15);
+		const columns = c.columns.filter(col => col.exf__EFSheet__c.substring(0, 15) === sheetId);
+		const rows = c.rows.filter(row => row.exf__EFSheet__c.substring(0, 15) === sheetId);
+		const cells = c.cells.filter(cell => cell.exf__EFSheet__c === sheetId);
+		console.log('ALL Cells : ' + JSON.stringify(c.cells));
+		console.log('sheet.Id : ' + sheetId);
+		console.log('Cells : ' + JSON.stringify(cells));
+		setExcelTopHeader(tableSheet, columns);
+		setExcelRows(tableSheet, rows, cells);
+		c.openedSheet = tableSheet;
+	} catch (e) {
+		_message('error', 'Generate Sheet Error : ' + e);
+	}
+};
+
+const setExcelTopHeader = (tableSheet, columns) => {
+	try {
+		tableSheet.letterHeaders = [{s: `width: 40px`, letter: ''}];
+		const columnsMap = columns.reduce((r, col) => {
+			r[col.exf__Index__c] = col.exf__Width__c;
+			return r;
+		}, {});
+		console.log('COL MAP:' + JSON.stringify(columnsMap));
+		generateExcelAlphabetArray(MIN_SHEET_WIDTH).forEach((letter, i) => {
+			const indicatedColumnWidth = columnsMap[i + 1];
+			console.log('col #' + (i + 1) + ' width: ' + indicatedColumnWidth);
+			const width = indicatedColumnWidth ? indicatedColumnWidth : defaultColumnWidth;
+			tableSheet.letterHeaders.push({s: `width: ${width}px`, letter});
+		});
+	} catch (e) {
+		_message('error', 'Set Excel Top Borders Error : ' + e);
+	}
+};
+
+const setExcelRows = (tableSheet, rows, cells) => {
+	try {
+		const tableRows = [];
+		const rowCellMatrix = getRowCellMatrix(cells);
+		const rowsMap = rows.reduce((r, row) => { // in the future style for whole row
+			r[row.exf__Index__c] = row.exf__Width__c;
+			return r;
+		}, {});
+		for (let xIdx = 0; xIdx < MIN_SHEET_HEIGHT; xIdx++) {
+			const tableRow = {numberHeader: {number: xIdx + 1}, cells: []};
+			for (let yId = 0; yId < MIN_SHEET_WIDTH; yId++) {
+				const cell = rowCellMatrix[xIdx + 1]?.[yId + 1];
+				const tableCell = {
+					idx: yId + 1,
+					value: cell ? getCellValue(cell) : '',
+					style: getCSSStyle(cell?.exf__EFStyle__c)
+				};
+				tableRow.cells.push(tableCell);
+			}
+			tableRows.push(tableRow);
+		}
+		tableSheet.rows = tableRows;
+	} catch (e) {
+		_message('error', 'Set Excel Rows Error : ' + e);
+	}
+};
+
+const getCellValue = (cell) => {
+	try {
+		console.log('Get Value for ' + cell.Name + ' => ' + cell.exf__EFDataSet__c);
+		if (!cell.exf__EFDataSet__c) return cell.exf__Value__c;
+		const sObject = c.sObjectsMap[cell.exf__EFDataSet__c][0];
+		console.log('sObject:' + JSON.stringify(sObject));
+		console.log('cell.DataSetField__c:' + cell.exf__DataSetField__c);
+		console.log('val:' + sObject[cell.exf__DataSetField__c]);
+		return sObject[cell.exf__DataSetField__c];
+	} catch (e) {
+		_message('error', 'Get Cell Value Error : ' + e);
+	}
+};
+
+/**
+ * TODO: Remove it to CSS classes
+ * @param styleId
+ * @return {string|undefined}
+ */
+const getCSSStyle = (styleId) => {
+	const style = styleMap[styleId];
+	if (!styleId || !style) return undefined;
+	//{"AlignmentHorizontal__c":"left","AlignmentVertical__c":"bottom","FillPatern__c":"none","FontBold__c":true,"FontItalic__c":false,"FontSize__c":16,"FontUnderline__c":false,"Id":"a3J55000000jKWTEA2","Name":"Header"}
+	let result = 'height: 100%; width: 100%; ';
+	if (style.exf__FillColor__c) result += `background-color: ${style.exf__FillColor__c}; `;
+	if (style.exf__FontColor__c) result += `color: ${style.exf__FontColor__c}; `;
+	if (style.exf__FontSize__c) result += `font-size: ${style.exf__FontSize__c}px; `;
+	return result;
+};
+
+
+/**
+ * Method returns object where key is row index value is object where key is cell index and value is cell
+ */
+const getRowCellMatrix = (cells) => {
+
+	const rowCellMatrix = {};
+	for (let i = 0; i < cells.length; i++) {
+		const cell = cells[i];
+		const {exf__EFRowIndex__c: rowIndex, exf__Index__c: cellIndex} = cell;
+		let cellObject = rowCellMatrix[rowIndex];
+		if (!cellObject) {
+			cellObject = {};
+			rowCellMatrix[rowIndex] = cellObject;
+		}
+		cellObject[cellIndex] = cell;
+	}
+	return rowCellMatrix;
+};
+
+/**
+ * Method returns excel letters array
+ */
+const generateExcelAlphabetArray = (length) => {
+	const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	const result = [];
+	let count = 0;
+	let letter = '';
+	while (result.length < length) {
+		if (count === alphabet.length) {
+			count = 0;
+			letter = letter + 'A';
+		}
+		if (letter === '') {
+			result.push(alphabet[count]);
+		} else {
+			result.push(letter + alphabet[count]);
+		}
+		count++;
+	}
+	return result;
+};
+
+export {setContext, generateTable};
